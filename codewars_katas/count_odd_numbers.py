220c8bfa61f42320463c356fca6fe39ba5a21f7a
Roksana Nikoopour <roksana.nikoopour@bt.com>
2019-11-11 13:22:07 +0000

    adding odd nos kata

---
+++ codewars_katas/count_odd_numbers.py
@@ -0,0 +1,40 @@
+'''Given a number n, return the number of positive odd numbers below n, EASY!
+
+oddCount(7) //=> 3, i.e [1, 3, 5]'''
+
+#Link to kata:   https://www.codewars.com/kata/count-odd-numbers-below-n/train/python 
+
+
+#i added in an if statement so it only runs when the  number is odd e.g. if x/2 gives a remainder
+
+
+def odd_count(n):
+    if n %2 != 0:
+        x = 1
+        condition = x+2
+        mylist=[1]
+        while condition < n:
+            #print(condition)
+            mylist.append(condition)
+            condition +=2
+        return len(mylist)
+    
+        print(mylist)
+        print("number of positive odd numbers below n:", len(mylist))
+    
+odd_count(7)
+
+
+def odd_count_spyder(n):
+    if n %2 != 0:
+        x = 1
+        condition = x+2
+        mylist=[1]
+        while condition < n:
+            #print(condition)
+            mylist.append(condition)
+            condition +=2
+        print(mylist)
+        print("number of positive odd number below n:", len(mylist))
+    
+odd_count_spyder(9)
