c36e39d625047cf3e05cebc439cbd5c9d0ec0627
Roksana Nikoopour <roksana.nikoopour@bt.com>
2019-12-02 14:43:42 +0000

    adding while loop

---
+++ while_loops.py
@@ -0,0 +1,32 @@
+#--------WHILE Loops ---------#
+
+'''Repeat this action until the condition is no longer satisfied'''
+
+
+#while condition:   while True / while False
+    #expression
+
+# while condition:
+#     pass
+
+#code
+# while condition:
+#     pass
+# #more code
+
+error=50
+while error >1:
+error = error/4 #you MUST UPDATE the error var!
+    print(error)
+
+1. Assign myvar
+2. add a while statement with a condition (e.g. greater than/less than)
+3. update myvar
+var = x
+while var > y
+var = z
+
+
+
+
+
