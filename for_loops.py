54b95dff8b90ed6356ac0dae460021e5582a5bb8
Roksana Nikoopour <roksana.nikoopour@bt.com>
2019-12-04 17:38:32 +0000

    updated for loops

--- for_loops.py
+++ for_loops.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 28 11:40:31 2019
-
-@author: 612436075
+For loops allow you to iterate over an object, this being a string, list, tuple or dictionary but NOT an int
 """
 #-------------FOR LOOPS-----------#
 
@@ -10,7 +8,7 @@ Created on Mon Jan 28 11:40:31 2019
 def fruit_loop():
     mylist = ["apple", "pear", "banana"]   
     for item in mylist:
-        print( item)
+        print(item)
         
 fruit_loop()
 
@@ -54,3 +52,105 @@ def move_in_steps():
         print(i)
         
 move_in_steps()
+
+#------printing out the index of each item ----------#
+
+print("\n for loop using enumerate to print out the index of each item ")
+
+def index_loop():
+    age = [12,24,32]
+    for index, height in enumerate(age):
+        print("index-" + str(index) + ": " + str(height))
+#to start at index 1 just do index+1 :)
+index_loop()
+
+#------For x in range---------#
+print("\n ------For x in range--------")
+#range(x,y) x INclusive and y EXclusive
+
+def for_in_range():
+    for i in range(0,10):
+        print(i)
+for_in_range()
+
+#to print sthing x times
+
+def print_multiple_times():
+    for i in range(5):
+        print("i must do my homework")
+
+print_multiple_times()
+
+
+print("\n for loop with conditional")
+
+def conditional_for():
+    ages=[18,22,12,8,39,50,45,17]
+    for age in ages:
+        if age <18:
+            print("Age", age, ": no entry")
+        else:
+            print("Age", age, ": entry permitted")
+
+conditional_for()
+
+#------Practical example of conditional for loop---------#
+print("\n ------Task--------")
+'''Calculate how many of group_a are under age and how many are allowed in to the club'''
+
+def task():
+    group_a=[18,22,12,8,39,50,45,17]
+    over_18 = []
+    under_age = []
+    for age in group_a:
+        if age <18:
+            under_age.append(age)
+            print("Age", age, ": no entry")
+        else:
+            over_18.append(age)
+            print("Age", age, ": entry permitted")
+    print("Over 18s:", len(over_18))
+    print("Under age:", len(under_age))
+task()
+
+#----------one-liners---------#
+#[ expression for item in list if conditional ]
+
+print("\n ------Task using tidy list comprehensions---------")
+def task_using_list_comprehension():
+    group_a=[18,22,12,8,39,50,45,17]
+    over_18 = [age for age  in group_a if age >=18 ]
+    under_age = [age for age in group_a if age <18]
+    
+    print("Over 18s:", len(over_18))
+    print("Under age:", len(under_age))
+
+task_using_list_comprehension()
+
+
+print("\n For loop iterating over dictionaries")
+
+mydict = {"person1": "john", "person2": "julie", "person3":"mary"}
+
+for k in mydict:
+    print("keys:", k)
+
+    #to print all keys:
+for k in mydict.keys():
+    print(k)
+    
+#to print all values:
+for v in mydict.values():
+    print(v)
+
+#to print keys ANDvalues:
+print("\n For loop iterating over dictionaries keys AND values")
+
+for k in mydict:
+    print(k, mydict[k])
+
+#this gives a tuple 
+print(mydict.items())
+
+for k,v in mydict.items():
+    print(k,v)
