91e10864d6bbde2e13cc7ba3eafd338a165e8b1b
Roksana Nikoopour <roksana.nikoopour@bt.com>
2019-12-04 16:33:25 +0000

    updated if else

--- if_else.py
+++ if_else.py
@@ -2,20 +2,58 @@
 """
 -Simple if/else statements
 -Ternary (one line) conditionals
--
+if condition:
+   do this
+then continue here regardless
 """
 
-#if condition:
-#    do this
-#else:
-#    do something else
+print("\n ------if statements-----")
+
+if 25<30:
+    print("25 is less than 30")
+#if this was not the case, the print block would NOT be executed
+
+#double equals is used to check equality 
+if not 25 == 26:
+    print("they are not equal")
+
+#note the difference between == and is
+if 2 == 2.0:
+    print("2 == 2.0")
+
+if 2 is 2.0:
+    print("2 is 2.0")
+#2 is NOT 2.0 but is == to 2.0
+
+print("\n ---------if var >> (if true)------")
+
+#in python all non-zero values are considered True, hence, this works as "if true".. execute code
+
+number=23
+if number:
+    print("this works as the var 'number' is  a non-zero value")
+#try this with number = 0 :) 
+print("\n ---------if and else statemements--------")
+'''
+if condition:
+    do this
+else:
+   do something else
+'''
+salary = 1000
+expenses = 500
+within_budget = False
+if salary-expenses>0:
+    within_budget = True
+
+print("am i within my budget?", within_budget)
 
 def conditionals():
     result = "pass"
     if result == "pass":
         print("well done")
     else:
-        print("sorry better luck next time")
+        print("sorry, better luck next time")
 
 def example1():
     code = int(input("enter code: "))
@@ -53,7 +91,7 @@ def conditionals_2():
 #conditionals_2()
 
 #---------------Ternary conditionals--------------#
-        
+print("\n------Ternary conditionals--------")      
 '''remove the need for multiline statements'''
 
 #condition_if_true IF condition ELSE condition_if_false
