593e0662c6815836906e979582a84c29e9644753
Roxey <32243415+roxey804@users.noreply.github.com>
2020-03-15 11:23:51 +0000

    Update break_continue_pass.py

--- break_continue_pass.py
+++ break_continue_pass.py
@@ -45,6 +45,10 @@ print("-------Continue keyword---------")
 time.sleep(1)   
 #continue will skip execution of the remaining lines of code and go back to the if statement  
 
+    if negative case:
+        continue
+    for positive case do X 
+
 nums=[1,2,3,4,5,6,7,8,9]
 
 for num in nums:
